45045d2a10b3e56ab4df07a21737f78db0114edb
Pedro-hmf0906 <pedrohmf51@gmail.com>
2026-03-10 12:09:55 -0300

    Otimização do código cpf

--- Python/Exercícios/CriarPrimeiro_SegundoDigitoDeUmCPF.py
+++ Python/Exercícios/CriarPrimeiro_SegundoDigitoDeUmCPF.py
@@ -1,81 +1,35 @@
 while True:
-    
     try:
-        lista_primeiros_num = []
-        cpf = ''
-        contador = 0
-        resultado_soma_mult_cpf = 0
+        numeros = []
 
-        while contador < 9:
-            valor = int(input("Digite um número: "))
-            
+        while len(numeros) < 9:
+            valor = int(input("Digite um número (0-9): "))
+
             if 0 <= valor <= 9:
-                lista_primeiros_num.append(valor)
-                contador += 1
-                
-                if contador == 3 or contador == 6:
-                    cpf += str(valor) + "." 
-                    
-                else: 
-                    cpf += str(valor)
-                    
+                numeros.append(valor)
             else:
-                print("Por favor, insira um valor de 0 à 9, tente novamente !")
-        
-        print(cpf)
+                print("Digite um número entre 0 e 9")
 
-        for i, numeros in enumerate(lista_primeiros_num):
-            peso = 10 - i
-            resultado_soma_mult_cpf += (peso * numeros)
+        def calcula_digito(lista, peso_inicial):
+            soma = 0
 
-        print('Resultado da soma:',resultado_soma_mult_cpf)
+            for i, numero in enumerate(lista):
+                soma += numero * (peso_inicial - i)
 
-        resultado_soma_mult_cpf *= 10
+            resultado = (soma * 10) % 11
+            return 0 if resultado > 9 else resultado
 
-        print("Soma multiplicada por 10:", resultado_soma_mult_cpf)
+        digito1 = calcula_digito(numeros, 10)
+        numeros.append(digito1)
 
-        resto_divisao = resultado_soma_mult_cpf % 11
+        digito2 = calcula_digito(numeros, 11)
+        numeros.append(digito2)
 
-        print("Resto da divisão por 11:", resto_divisao)
+        cpf = "{}{}{}.{}{}{}.{}{}{}-{}{}".format(*numeros)
 
-        if resto_divisao > 9:
-            lista_primeiros_num.append(0)
-            cpf += "-0"
-        else:
-            lista_primeiros_num.append(resto_divisao)
-            cpf += "-" + str(resto_divisao)
-
-        print(lista_primeiros_num)
-        print(cpf)
-
-        resultado_soma_mult_cpf = 0
-
-        for i, numeros in enumerate(lista_primeiros_num):
-            peso = 11 - i
-            resultado_soma_mult_cpf += (peso * numeros)
-
-        print('Resultado da soma:',resultado_soma_mult_cpf)
-
-        resultado_soma_mult_cpf *= 10
-
-        print("Soma multiplicada por 10:", resultado_soma_mult_cpf)
-
-        resto_divisao = resultado_soma_mult_cpf % 11
-
-        print("Resto da divisão por 11:", resto_divisao)
-
-        if resto_divisao > 9:
-            lista_primeiros_num.append(0)
-            cpf += "0"
-        else:
-            lista_primeiros_num.append(resto_divisao)
-            cpf += str(resto_divisao)
-
-        print(lista_primeiros_num)
-        print(cpf)
+        print("CPF gerado:", cpf)
 
         break
-    
+
     except ValueError:
-        print("Por favor, digite somente números !")
-        continue
+        print("Digite apenas números!")
